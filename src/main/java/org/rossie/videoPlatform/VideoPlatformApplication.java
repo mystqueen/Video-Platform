ef84f18c3c518939dd3e0eff3b72372372848be7
Tetteh Rose <queenefya669@gmail.com>
2024-07-09 04:04:40 -0700

    fix:secret key github restrictions

--- src/main/java/org/rossie/videoPlatform/VideoPlatformApplication.java
+++ src/main/java/org/rossie/videoPlatform/VideoPlatformApplication.java
@@ -1,5 +1,6 @@
 package org.rossie.videoPlatform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,8 @@ public class VideoPlatformApplication {
 
     public static void main(String[] args) {
 
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("azure.storage.account-key", dotenv.get("AZURE_STORAGE_ACCOUNT_KEY"));
         SpringApplication.run(VideoPlatformApplication.class, args);
     }
 
